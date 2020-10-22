31c1620281ee1e7f000d1b0261516181b1071fbf
newhorizon-tech <alaukikinbox@gmail.com>
2020-10-22 13:47:45 +0530

    Refactored code into different methods

--- main.rb
+++ main.rb
@@ -1,23 +1,39 @@
 # Stock Picker
 
 def stock_picker(arr)
-  buy=0
-  sell=0
-  profit=0
+  buy = 0
+  sell = 0
+  profit = 0
   arr.each_with_index do |p, d|
     arr.each_with_index do |price, day|
-        if (d - day).positive? and p - price > profit
-          profit = p - price
-          buy = day
-          sell = d
-          puts "buy #{buy}: sell #{sell} and profit is #{profit}"
-        end
+      next unless (d - day).positive? and p - price > profit
+      profit = p - price
+      buy = day
+      sell = d
     end
   end
-  puts "Buy stock on day #{buy} for $#{arr[buy]} and sell it on day #{sell} for $#{arr[sell]} and make $#{profit} profit "
+  [buy, sell]
 end
 
-stock_price=[]
-6.times { stock_price.push(rand(0..100)) }
-p stock_price
-stock_picker(stock_price)
+def print_stock_prices(arr)
+  arr.each_with_index do |price, day|
+    puts "Day #{day + 1}: $#{price}"
+  end
+end
+
+def print_profit(arr, buy, sell)
+  profit = arr[sell] - arr[buy]
+  print "Buy stock on day #{buy + 1} for $#{arr[buy]} and sell it on day #{sell + 1} for $#{arr[sell]}"
+  print " and make $#{profit} profit "
+  print "\n"
+end
+
+stock_price = []
+10.times { stock_price.push(rand(0..100)) }
+stock_price[0] = 100
+stock_price[9] = 0
+
+stock_result = stock_picker(stock_price)
+
+print_stock_prices(stock_price)
+print_profit(stock_price, stock_result[0], stock_result[1])
